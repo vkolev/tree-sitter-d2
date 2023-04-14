42f07c1167734e19260f532551c782bcf8b57916
Vladimir Kolev <vladimir.r.kolev@gmail.com>
2023-04-14 15:50:51 +0200

    Update Package.swift structure

--- Package.swift
+++ Package.swift
@@ -1,43 +1,41 @@
-// swift-tools-version:5.6
+// swift-tools-version:5.3
 import PackageDescription
 
 let package = Package(
     name: "TreeSitterD2",
-    platforms: [.macOS(.v10_13), .iOS(v11)],
+    platforms: [.macOS(.v10_13), .iOS(.v11)],
     products: [
         .library(name: "TreeSitterD2", targets: ["TreeSitterD2"]),
     ],
     dependencies: [],
     targets: [
-        .target(
-            name: "TreeSitterD2",
-            path: ".",
-            exclude: [
-                "assets",
-                "binding.gyp",
-                "bindings",
-                "Cargo.toml",
-                "Cargo.lock",
-                "dev",
-                "examples",
-                "flake.lock",
-                "flake.nix",
-                "grammer.json"
-                "Makefile",
-                "package.json"
-                "README.md"
-                "src/grammer.json",
-                "src/node-types.json",
-            ],
-            sources: [
-                "src/parser.c",
-                "src/scanner.cc",
-            ],
-            resources: [
-                .copy("queries")
-            ],
-            publicHeadersPath: "bindings/swift",
-            cSettings: [.headerSearchPath("src")]
-        )
+        .target(name: "TreeSitterD2",
+                path: ".",
+                exclude: [
+                    "assets",
+                    "binding.gyp",
+                    "bindings",
+                    "Cargo.toml",
+                    "Cargo.lock",
+                    "dev",
+                    "examples",
+                    "flake.lock",
+                    "flake.nix",
+                    "grammer.json"
+                    "Makefile",
+                    "package.json"
+                    "README.md"
+                    "src/grammer.json",
+                    "src/node-types.json",
+                ],
+                sources: [
+                    "src/parser.c",
+                    "src/scanner.cc",
+                ],
+                resources: [
+                    .copy("queries")
+                ],
+                publicHeadersPath: "bindings/swift",
+                cSettings: [.headerSearchPath("src")])
     ]
 )
